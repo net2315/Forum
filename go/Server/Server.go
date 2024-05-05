c837365d78f8fc15129c99584021601fb600306e
noumouaw <oumouawa.ndiaye@ynovcom>
2024-05-05 20:25:08 +0200

    Login and registration page

--- go/Server/Server.go
+++ go/Server/Server.go
@@ -12,6 +12,11 @@ func HandleFunc() {
 
 	http.HandleFunc("/", Home)
 
+	http.HandleFunc("/login", Login)
+
+	http.HandleFunc("/register", Register)
+
+
 	http.Handle("/media/", http.StripPrefix("/media/", http.FileServer(http.Dir("./media"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./assets/css/"))))
 
@@ -25,6 +30,15 @@ func HandleFunc() {
 func Home(w http.ResponseWriter, r *http.Request) { //affiche la page du menu principal
 	renderTemplate(w, "assets/html/Accueil")
 }
+
+func Login(w http.ResponseWriter, r *http.Request) {
+    renderTemplate(w, "assets/html/login")
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+    renderTemplate(w, "assets/html/register")
+}
+
 
 func renderTemplate(w http.ResponseWriter, tmpl string) { //Parse le fichier html et envoi les informations au client
 	t, err := template.ParseFiles("./" + tmpl + ".html")
