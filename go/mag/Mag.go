657eb20414a85ada3ac65647763c4037f89013a1
noumouaw <oumouawa.ndiaye@ynovcom>
2024-05-30 17:07:13 +0200

    adding post user and category structures

--- go/mag/Mag.go
+++ go/mag/Mag.go
@@ -5,3 +5,28 @@ type Categorie struct {
 	Nom         string
 	Description string
 }
+
+type Post struct {
+	ID         int
+	CategorieID int
+	Texte      string
+	DateHeure  string 
+	Photo      []byte
+}
+
+type User struct {
+	ID            int
+	Pseudo        string
+	Mail          string
+	MotDePasse    string
+	IDPost        int
+	IDCommentaire int
+}
+
+type Comment struct {
+	ID         int
+	UserID     int
+	Texte      string
+	DateHeure  string
+	Likes      int
+}
