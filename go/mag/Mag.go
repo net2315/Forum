4f84173a571d2c2a97c69025f3580d56acbef83e
net2315 <110617938+net2315@users.noreply.github.com>
2024-06-18 21:01:14 +0200

    add new table in struct

--- go/mag/Mag.go
+++ go/mag/Mag.go
@@ -1,16 +1,20 @@
 package mag
 
+import "time"
+
 type Categorie struct {
 	ID          int
 	Nom         string
+	Photo       string
 	Description string
 }
 
 type Post struct {
 	ID          int
 	CategorieID int
+	Titre       string
 	Texte       string
-	DateHeure   string
+	DateHeure   time.Time
 	Photo       string
 	Likes       int
 	Comments    []Comment
@@ -20,13 +24,14 @@ type Comment struct {
 	ID        int
 	UserID    int
 	Texte     string
-	DateHeure string
+	DateHeure time.Time
 	Likes     int
 	PostID    int
 }
 
 type User struct {
 	ID            int
+	Photo         string
 	Pseudo        string
 	Mail          string
 	MotDePasse    string
